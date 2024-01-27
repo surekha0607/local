e47cadbad18e63113fe4a814b89119538a7c2938
surekha0607 <surekhasharma568@gmail.com>
2024-01-27 11:22:03 +0530

    java file added

---
+++ file.java
@@ -0,0 +1,24 @@
+package Demo;
+
+import java.util.Scanner;
+
+public class IntegerArraySumCalculator {
+
+	public static void main(String[] args) {
+
+		int[] summation = new int[5];
+		int sum = 0;
+
+		Scanner sc = new Scanner(System.in);
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Enter an integer:");
+			summation[i] = sc.nextInt();
+			sum = sum + summation[i];
+
+		}
+		sc.close();
+
+		System.out.println("Grand Total: " + sum);
+
+	}
+}
